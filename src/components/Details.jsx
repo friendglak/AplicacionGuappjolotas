0ea8a8edfbd83b1196407614c8394da02232f312
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-07-03 16:32:34 -0500

    Get api y pintando los productos

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { useParams } from 'react-router-dom'
 
 const Details = () => {
+    const { id } = useParams();
+    const [producto, setProducto] = useState([]);
+
+
+    useEffect(() => {
+        const getProducto = async () =>{
+            const url = `https://api-guapjolotas-2021.vercel.app/productos/${id}`
+            const res = await fetch(url);
+            const data = await res.json()
+            setProducto(data)
+        }
+        getProducto()
+    }, [])
+
+
+
     return (
         <div>
-            <h1>Soy detalles</h1>
+            <img src={producto.imagen} alt="" />
         </div>
     )
 }
