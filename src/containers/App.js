51e804629aabd2f72892c346b9443645f97b0dc3
FriendGlak <camilojheans@gmail.com>
2021-07-12 16:58:04 -0500

    Rutas corregidas v2

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,39 @@
-import React from 'react';
-import GlobalStyle from '../globalStyles.js';
-import Navbar from '../components/Navbar.jsx';
-import Footer from '../components/Footer.jsx';
-import Inicio from '../components/Inicio.jsx'
-import Login from '../components/Login.jsx';
-import Details from '../components/Details.jsx';
-import Carrito from '../components/Carrito.jsx';
+import React from "react";
+import GlobalStyle from "../globalStyles.js";
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import Inicio from "../components/Inicio.jsx";
+import Login from "../components/Login.jsx";
+import Details from "../components/Details.jsx";
+import Carrito from "../components/Carrito.jsx";
 //import ScrollToTop from '../components/ScrollToTop'
 
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import Waiting from '../components/Waiting.jsx';
-
-
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   return (
-
     <Router>
       <GlobalStyle />
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Waiting}/>
-        <Route path="/inicio"  component={Inicio} />
+        <Route exact path="/AplicacionGuappjolotas/">
+          <Redirect to="/inicio" component={Inicio} />
+        </Route>
+        <Route exact path="/">
+          <Redirect to="/inicio" component={Inicio} />
+        </Route>
+        <Route path="/inicio" exact component={Inicio} />
         <Route path="/ingresar" component={Login} />
         <Route path="/detalles/:id" component={Details} />
         <Route path="/carrito" component={Carrito} />
       </Switch>
       <Footer />
-    </Router >
-
+    </Router>
   );
 };
 
