f9c6f56fbc61e1feb3973708df378caa7bb5faab
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-07-13 15:01:27 -0500

    Eliminando confi de stripe

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react'
 import GetLocalStorage from '../helpers/GetLocalStorage'
 import PaintProductoCarrito from '../hooks/PaintProductoCarrito';
 import { DivButton, ButtonPay } from '../style/CarritoStyles';
-import { loadStripe } from "@stripe/stripe-js";
-const stripePromise = loadStripe("pk_test_51J3WxqBbP3JGRmwnfVffXQE2tdimeAg5wrqN2RYl4z2eJcQO92U7xWYUILSmliy9Ay6wvAAOYIgKdv9GPbmOaK8G003KgExXWh");
+
 // import ProductosPasarelaStripe from '../hooks/ProductosPasarelaStripe';
 
 
@@ -19,23 +18,6 @@ const Carrito = () => {
 
     const _handlePagar = async () =>{
         console.log("Estoy pagando")
-        const productsObject = {
-            carrito
-        }
-        const stripe = await stripePromise;
-
-		const response = await fetch(`${env.URL_SERVER}/create-checkout-session`, {
-            body: JSON.stringify(productsObject),
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-        const session = await response.json();
-
-        const result = await stripe.redirectToCheckout({
-        sessionId: session.id,
-        });
     }
     return (
         <div>
